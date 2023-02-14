f90b3f9665ec7fe9fbfeb5cc682f2a3e50c338af
Talym <121556351+TopSoilPro@users.noreply.github.com.com>
2023-02-14 18:44:13 +1100

    Add colour changing on mouse hover
    Made the grid squares reactively change colour depending on the
    presence of the mouse over the object or not

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
+const MAX_DIMENSION = 100;
+const DEFAULT_DIMENSION = 4;
+
 // Fills a grid with squares 
-function createGrid() {
-    const GRID_DIMENSIONS = 4;
-    const TOTAL_SQUARES = 16;
+function createGrid(dimensions) {
     const grid = document.querySelector('.grid');
 
     // Creating the columns of the grid
-    for (let i = 0; i < GRID_DIMENSIONS; i++) {
+    for (let i = 0; i < dimensions; i++) {
         const gridColumn = document.createElement('div');
         gridColumn.classList.add('col');
         // Creating the squares for each column
-        for (let j = 0; j < GRID_DIMENSIONS; j++) {
+        for (let j = 0; j < dimensions; j++) {
             const gridSquare = document.createElement('div');
             gridSquare.classList.add('gridSquare');
             gridColumn.appendChild(gridSquare);
@@ -18,4 +19,23 @@ function createGrid() {
     }
 }
 
-createGrid();
+// Adds events for changing square colour on mouse hover
+function createHoverEvents() {
+    const squares = document.querySelectorAll('.gridSquare');
+
+    squares.forEach(square =>
+        square.addEventListener('mouseover', function(e) {
+            //e.target.style.backgroundColor = "black";
+            e.target.classList.add('hovered');
+        })
+    );
+
+    squares.forEach(square =>
+        square.addEventListener('mouseout', function(e) {
+            //e.target.style.backgroundColor = "white";
+            e.target.classList.remove('hovered');
+        }))
+}
+
+createGrid(DEFAULT_DIMENSION);
+createHoverEvents();
